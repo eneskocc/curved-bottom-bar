afe4fb43036617b9f80cd7e21bb75f365aeaeb8e
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-04-06 15:51:41 +0300

    Update TabBar.tsx

--- TabBar.tsx
+++ TabBar.tsx
@@ -1,10 +1,6 @@
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { colors } from '../../theme/colors';
-import { customNavigation } from '../../navigation/RouteNavigation';
-import { NAVIGATIONTYPE } from '../../constants';
-import { useTranslation } from 'react-i18next';
 import Svg, { Circle, Path } from 'react-native-svg';
 import { getPathDown } from './path';
 import { scale } from 'react-native-size-scaling';
@@ -17,7 +13,6 @@ interface Props {
 const TabBar: React.FC<Props> = ({
     active
 }) => {
-    const { t } = useTranslation();
     const returnPath = getPathDown(width, 65, 65, false, "CENTER")
     
     return (
@@ -28,32 +23,23 @@ const TabBar: React.FC<Props> = ({
             <View style={styles.footer}>
                 
                 <TouchableOpacity
-                    onPress={() => {
-                        customNavigation(NAVIGATIONTYPE.PUSH, 'ProfileSettingsScreen', {});
-                    }}
                     style={[styles.item, {  }]}>
-                    <Icon name={active == 'user' ? "user-alt" : "user"} size={20} color={active == 'user' ? colors.blue : colors.grayText} />
-                    <Text style={{ color: active == 'user' ? colors.blue : colors.grayText, fontSize: 10,fontFamily:'Poppins' }}>{t('langKey:profil')}</Text>
+                    <Icon name={active == 'user' ? "user-alt" : "user"} size={20} color={active == 'user' ? 'blue' : 'gray'} />
+                    <Text style={{ color: active == 'user' ? 'blue' : colors.grayText, fontSize: 10,fontFamily:'Poppins' }}>profil</Text>
                 </TouchableOpacity>
 
 
 
                 <TouchableOpacity
-                    onPress={() => {
-                        customNavigation(NAVIGATIONTYPE.PUSH, 'HomeScreen', {});
-                    }}
                     style={[styles.item, {  borderTopRightRadius: 32 }]}>
-                    <Icon name={active == 'comments' ? "comments" : "comments"} size={20} color={active == 'comments' ? colors.blue : colors.grayText} />
-                    <Text style={{ color: active == 'comments' ? colors.blue : colors.grayText, fontSize: 10 ,fontFamily:'Poppins'}}>{t('langKey:mesajlar')}</Text>
+                    <Icon name={active == 'comments' ? "comments" : "comments"} size={20} color={active == 'comments' ? 'blue' : 'gray'} />
+                    <Text style={{ color: active == 'comments' ? 'blue' : 'gray', fontSize: 10 ,fontFamily:'Poppins'}}mesajlar</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity
-                    onPress={() => {
-                        customNavigation(NAVIGATIONTYPE.PUSH, 'HomeScreen', {});
-                    }}
+                <TouchableOpacity        
                     style={[styles.item, { zIndex: -20 }]}>
                     <View style={{ width: 90, height: 90, borderRadius: 45, borderBottomRightRadius: 45, alignItems: 'center', justifyContent: 'center', position: 'relative', top:5, zIndex: 1 }}>
-                        <View style={{ width: 70, height: 70, borderRadius: 35, alignItems: 'center', justifyContent: 'center', backgroundColor: active == 'home' ? colors.blue : colors.white, }}>
+                        <View style={{ width: 70, height: 70, borderRadius: 35, alignItems: 'center', justifyContent: 'center', backgroundColor: active == 'home' ? 'blue' : 'white', }}>
                             <Image style={{ width: 45, height: 45, resizeMode: 'contain' }} source={active == 'home' ? require('../../../assets/images/homeIconWhite.png') : require('../../../assets/images/homeIconBlue.png')} />
                         </View>
                     </View>
@@ -64,24 +50,18 @@ const TabBar: React.FC<Props> = ({
                         </Svg>
 
                     </View>
-                    <View style={{ zIndex: 4, position: 'relative', top: -56 }}><Text style={{ color: colors.grayText, fontSize: 10,fontFamily:'Poppins' }}>{t('langKey:anasayfa')}</Text></View>
+                    <View style={{ zIndex: 4, position: 'relative', top: -56 }}><Text style={{ color: 'gray', fontSize: 10,fontFamily:'Poppins' }}>anasayfa</Text></View>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    onPress={() => {
-                        customNavigation(NAVIGATIONTYPE.PUSH, 'DashboardScreen', {});
-                    }}
                     style={[styles.item, {  borderTopLeftRadius: 32 }]}>
-                    <Icon name={active == 'calendar' ? "calendar-alt" : "calendar-alt"} size={20} color={active == 'calendar' ? colors.blue : colors.grayText} />
-                    <Text style={{ color: active == 'calendar' ? colors.blue : colors.grayText, fontSize: 10,fontFamily:'Poppins' }}>{t('langKey:randevularim')}</Text>
+                    <Icon name={active == 'calendar' ? "calendar-alt" : "calendar-alt"} size={20} color={active == 'calendar' ? 'blue' : 'gray'} />
+                    <Text style={{ color: active == 'calendar' ? 'blue' : 'gray', fontSize: 10,fontFamily:'Poppins' }}>randevularim</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress={() => {
-                        customNavigation(NAVIGATIONTYPE.PUSH, 'DashboardScreen', {});
-                    }}
                     style={[styles.item, { }]}>
-                    <Icon name={active == 'file' ? "file-alt" : "file-alt"} size={20} color={active == 'file' ? colors.blue : colors.grayText} />
-                    <Text style={{ color: active == 'file' ? colors.blue : colors.grayText, fontSize: 10,fontFamily:'Poppins' }}>{t('langKey:tibbi_bilgilerim')}</Text>
+                    <Icon name={active == 'file' ? "file-alt" : "file-alt"} size={20} color={active == 'file' ? 'blue' : 'gray'} />
+                    <Text style={{ color: active == 'file' ? 'blue' :'gray', fontSize: 10,fontFamily:'Poppins' }}>tibbi bilgilerim</Text>
                 </TouchableOpacity>
             </View>
             
